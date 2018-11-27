bf2c01bfc6153faecdc579c791bb595d114519c2
MohuBackup <backup@mohu.club>
2018-11-27 15:25:44 +0800

    更新

--- modify_html.js
+++ modify_html.js
@@ -5,24 +5,28 @@ const path = require("path")
 const backupType = "question"
 const baseFileName = `../backups/${backupType}s/html/`
 
-const matomoScript = /<script type="text\/javascript">\s+var _paq = _paq(\s|.|\n)+?<\/script>/g
-const crondScriptDiv = /<div style="display:none;" id="__crond">(\s|.)+?<\/div>/g
+const matomoScript = /<script type="text\/javascript">\s+var _paq = _paq(.|\n)+?<\/script>/g
+const crondScriptDiv = /<div style="display:none;" id="__crond">(.|\n)+?<\/div>/g
 const UrlBaseElement = /<base.+?-->/g
-const archiveURL = /https:\/\/.*?www.mohu.club\//
+const archiveURL0 = /(\/im_\/)?https:\/\/.*?www.mohu.club\//g
+const archiveURL1 = /https:([\w./:]+?(http(s)?))+/g
 
 
 const process = async (qid) => {
 
+    const filepath = path.normalize(`${baseFileName}/${qid}.html`)
+
     const html = (
-        await fs.readFile(path.normalize(`${baseFileName}/${qid}.html`))
+        await fs.readFile(filepath)
     ).toString()
 
-    html.replace(matomoScript, "")
+    const newHTML = html.replace(matomoScript, "")
         .replace(crondScriptDiv, "")
         .replace(UrlBaseElement, "")
-        .replace(archiveURL, "/")
+        .replace(archiveURL0, "/")
+        .replace(archiveURL1, "$2")
 
-
+    await fs.writeFile(filepath, newHTML)
 
 }
 
