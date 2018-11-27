5881f1f19d9378f6c5aee6731ea2535ec5316805
MohuBackup <backup@mohu.club>
2018-11-27 16:22:16 +0800

    更新

--- modify_html.js
+++ modify_html.js
@@ -5,8 +5,8 @@ const path = require("path")
 const backupType = "question"
 const baseFileName = `../backups/${backupType}s/html/`
 
-const matomoScript = /<script type="text\/javascript">\s+var _paq = _paq(.|\n)+?<\/script>/g
-const crondScriptDiv = /<div style="display:none;" id="__crond">(.|\n)+?<\/div>/g
+const matomoScript = /<script type="text\/javascript">\s+var _paq = _paq(.|\n|\s)+?<\/script>/g
+const crondScriptDiv = /<div style="display:none;" id="__crond">(.|\n|\s)+?<\/div>/g
 const UrlBaseElement = /<base.+?-->/g
 const archiveURL0 = /(\/im_\/)?https:\/\/.*?www.mohu.club\//g
 const archiveURL1 = /https:([\w./:]+?(http(s)?))+/g
@@ -16,9 +16,7 @@ const process = async (qid) => {
 
     const filepath = path.normalize(`${baseFileName}/${qid}.html`)
 
-    const html = (
-        await fs.readFile(filepath)
-    ).toString()
+    const html = await fs.readFile(filepath, "utf-8")
 
     const newHTML = html.replace(matomoScript, "")
         .replace(crondScriptDiv, "")
@@ -29,4 +27,13 @@ const process = async (qid) => {
     await fs.writeFile(filepath, newHTML)
 
 }
+
+fs.readdirSync(baseFileName)
+    .map((f) => path.parse(f).name)
+    .filter((id) => Number.isInteger(+id))
+    .sort((a, b) => (a | 0) - (b | 0))
+    .forEach((qid) => {
+        process(qid)
+    })
+
 
