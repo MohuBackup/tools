7e6b77a3676fc1bb2b33e32c9ad15a45c68d6b47
MohuBackup <backup@mohu.club>
2018-11-30 16:35:14 +0800

    更新

--- get_user_img_data.js
+++ get_user_img_data.js
@@ -1,47 +1,66 @@
-
+// @ts-check
 const fs = require("fs-extra")
-const path = require("path")
-const { getAllQidsThen } = require("./util")
+const { JSDOM } = require("jsdom")
+const { getAllQidsThen, flat } = require("./util")
 
 const backupTypes = ["article", "question"]
-const jsonFileName = "../backups/uploads.json"
+const jsonFilePath = "../backups/uploads.json"
 
-const imgReg = /\/uploads\/.+?\.(jpg|jpeg|png|gif)/g
+/** @typedef {{ src: string; alt: string; title: string; }} UserUploadsItem */
 
-let allImgs = new Set()
+/**
+ * @param {string} baseFilePath 
+ * @param {number} qid 
+ * @returns {Promise<UserUploadsItem[]>}
+ */
+const handler = async (baseFilePath, qid) => {
+    const html = await fs.readFile(`${baseFilePath}/${qid}.html`, "utf-8")
+    const { window: { document } } = new JSDOM(html)
 
-const handler = async (baseFileName, qid) => {
-    const filepath = path.normalize(`${baseFileName}/${qid}.html`)
-    const html = await fs.readFile(filepath, "utf-8")
+    /** @type {NodeListOf<HTMLImageElement>} */
+    const imgs = document.querySelectorAll("img[title]")
 
-    const imgs = html.match(imgReg)
+    if (imgs.length == 0) return
 
-    if (!imgs) return
+    console.log(imgs.length)
 
-    // console.log(imgs.length)
+    return [...imgs].map((img) => {
+        const { src, alt, title } = img
 
-    imgs.forEach((img) => {
-        allImgs.add(img)
+        return {
+            src,
+            alt,
+            title
+        }
     })
 }
 
+/** @type {UserUploadsItem[]} */
+let allImgs = []
+
 backupTypes.forEach(async (backupType) => {
-    const baseFileName = `../backups/${backupType}/`
+    const baseFilePath = `../backups/${backupType}/`
 
-    const cb = (qid) => handler(baseFileName, qid)
+    const cb = (qid) => handler(baseFilePath, qid)
 
-    await Promise.all(
-        getAllQidsThen(baseFileName, cb)
+    const imgs = await Promise.all(
+        getAllQidsThen(baseFilePath, cb)
     )
 
-    console.log(allImgs.size)
+    allImgs.push(...flat(imgs))
 
-    fs.writeFileSync(
-        jsonFileName,
-        JSON.stringify(
-            [...allImgs].sort(),
-            null, 4
-        )
-    )
+    const output = allImgs.sort((a, b) => {
+        if (a.src < b.src) {
+            return -1
+        } else if (a.src > b.src) {
+            return 1
+        } else {
+            return 0
+        }
+    }).filter((x) => {
+        return !!x && !x.src.startsWith("/ueditor/") && !x.src.startsWith("/static/")
+    })
+
+    await fs.writeJSON(jsonFilePath, output, { spaces: 4 })
 
 })
