8825c83a9c12780dc27b018a3eadf1e09dfcfa98
MohuBackup <backup@mohu.club>
2018-12-02 15:01:31 +0800

    使用moment.js

--- absolutize_time.js
+++ absolutize_time.js
@@ -1,6 +1,7 @@
 // @ts-check
 const fs = require("fs-extra")
 const path = require("path")
+const moment = require("moment-timezone")
 
 
 const baseFilePath = "../backups/question"
@@ -30,20 +31,6 @@ const getArchiveTimeData = () => {
 const archiveTimeData = getArchiveTimeData()
 
 /**
- * 暴力格式化时间 
- * @param {Date} t 
- */
-const formatTime = (t) => {
-
-    // const timeZoneOffset = (-new Date().getTimezoneOffset()) * 60 * 1000
-    const timeZoneOffset = 8 * 60 * 60 * 1000  // UTC+08:00
-
-    const d = new Date(t.getTime() + timeZoneOffset).toISOString()
-
-    return d.match(/\d{4}(-\d{2}){2}/)[0] + " " + d.match(/\d{2}:\d{2}/)[0]
-}
-
-/**
  * @param {number} qid 
  * @returns {Promise<void>}
  */
@@ -54,18 +41,18 @@ const absolutizeTime = async (qid) => {
     /**
      * @param {string} match 
      * @param {string} n 
-     * @param {string} unit 
+     * @param { "天" | "小时" } unit_zh 
      */
-    const replacer = (match, n, unit) => {
+    const replacer = (match, n, unit_zh) => {
+        const unit = unit_zh == "天" ? "days" : "hours"  // 翻译时间单位
+
         const archiveTime = archiveTimeData[qid]
 
-        const ms = unit == "天"
-            ? 24 * 60 * 60 * 1000  // 一天的毫秒数
-            : 60 * 60 * 1000  // 一小时的毫秒数
+        const absTime = moment(archiveTime).subtract((+n), unit)
 
-        const absTime = new Date(archiveTime.getTime() - (+n) * ms)
+        const formatted = absTime.tz("Asia/Shanghai").format("YYYY-MM-DD HH:mm")
 
-        return match.replace(/>.+?</, `>${formatTime(absTime)}<`)
+        return match.replace(/>.+?</, `>${formatted}<`)
     }
 
     const output = html.replace(r1, replacer)
