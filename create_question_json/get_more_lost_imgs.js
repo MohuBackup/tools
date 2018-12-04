caa7c1cd73bf216ffa14b0751429da98f65136c0
MohuBackup <backup@mohu.club>
2018-12-04 21:37:18 +0800

    更新

---
+++ create_question_json/get_more_lost_imgs.js
@@ -0,0 +1,71 @@
+// @ts-check
+const fs = require("fs-extra")
+const { getAllQidsThen } = require("../util")
+
+const backupType = "question"
+const baseFilePath = `../../json/${backupType}`
+
+const r0 = /(\/uploads\/(\w|\.|\/)+)/g
+const r1 = /http(s)?:\/\/archive.is\/o\/\w{5}\/http(s)?:\/\/(www\.)?mohu(1|2)?.(club|tw|tk|ml)(\/uploads\/(\w|\.|\/)+)/g
+const r2 = /http(s)?:\/\/archive.is\/\w{5}\/(\w+\.\w+)/g
+
+const replacer = () => { }
+
+/**
+ * @param {number} qid 
+ */
+const handler = async (qid) => {
+    const jsonFilePath = `${baseFilePath}/${qid}.json`
+    const input = await fs.readFile(jsonFilePath, "utf-8")
+
+    const m = input.match(r1)
+
+    if (!m) return
+
+    /** @type {string[]} */
+    const saveToPaths = m.reduce((l, x) => {
+        return l.concat(
+            x.match(r0)[0]
+        )
+    }, [])
+
+    const downloadURLs = input.match(r2)
+
+    if (saveToPaths.length != downloadURLs.length) {
+        console.error(jsonFilePath + " failed")
+        return
+    }
+
+    /** @type {(import("../get_user_img").imgDataItem)[]} */
+    const imgs = downloadURLs.map(
+        /** @return {[string, string]} */
+        (x, i) => {
+            return [
+                x,              // 下载地址
+                saveToPaths[i]  // 保存路径
+            ]
+        }
+    )
+
+    console.log(imgs)
+
+    // if (output != input) {
+    //     console.log(jsonFilePath)
+    //     fs.writeFile(jsonFilePath, output)
+    // }
+
+    return imgs
+}
+
+Promise.all(
+    [handler(1259)]
+    // getAllQidsThen(baseFilePath, handler)
+).then((allImgs) => {
+
+    const lostImgs = allImgs.filter(x => {
+        return !!x
+    }).reduce((l, x) => {
+        return l.concat(x)
+    }, [])
+
+})
